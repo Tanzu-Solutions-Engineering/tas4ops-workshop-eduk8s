3c248b5ca05c0c7552fde7f3e43dbdc7d053fa15
Luciano Silva <lsilva@pivotal.io>
2021-02-02 15:59:07 -0500

    Update gitignore to exclude certs and keys

--- config/deploy-sessions.sh
+++ config/deploy-sessions.sh
@@ -5,13 +5,24 @@
 # 2. Setup Ingress Controller, e.g. Nginx -  
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.43.0/deploy/static/provider/cloud/deploy.yaml
 # NGinx docs: Install NGINX Ingress Controller (https://kubernetes.github.io/ingress-nginx/deploy/#azure)
+# Deploy Cert-Manager:
+# kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml          
+# Create cert with letsencrypt and cert-manager:
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-an-nginx-ingress-with-cert-manager-on-digitalocean-kubernetes
+# Create secret with new cert in eduk8s namespace:
+# kubectl create secret tls -n eduk8s workshops-tls --cert=./workshops.crt --key=./workshops.key
 
 kubectl apply -k "github.com/eduk8s/eduk8s?ref=20.12.03.1"
 
 sleep 2
 
-kubectl set env deployment/eduk8s-operator -n eduk8s INGRESS_DOMAIN=eduk8s.azure.yourcompany.pw
+kubectl create secret tls -n eduk8s yourcompanyku-tls --cert=./yourcompany.crt --key=./yourcompany.key
+
+kubectl set env deployment/eduk8s-operator -n eduk8s INGRESS_DOMAIN=yourcompany.pw
 kubectl set env deployment/eduk8s-operator -n eduk8s INGRESS_CLASS=nginx
+kubectl set env deployment/eduk8s-operator -n eduk8s INGRESS_SECRET=yourcompany-tls
+
+kubectl get secrets -n eduk8s
 
 sleep 5
 
